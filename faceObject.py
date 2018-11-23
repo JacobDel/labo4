b64aa84e9c0a701d46590f730e420a8a694d9b70
Jacob <jacob.delabie@hotmail.com>
2018-11-23 15:33:32 +0100

    face recogn

---
+++ faceObject.py
@@ -0,0 +1,10 @@
+class Face:
+    startX=0
+    startY = 0
+    height = 0
+    width = 0
+    def __init__(self,x,y,h,w):
+        self.startX = x
+        self.startY = y
+        self.width = w
+        self.height = h
