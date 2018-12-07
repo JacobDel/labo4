b25b43ac74fd03e5ca31b494cc751b2c90a02e3c
Martijn Gevaert <martijn.gevaert@skynet.be>
2018-12-07 16:05:09 +0100

    created new better working tracking class

--- OpenCVTracker.py
+++ OpenCVTracker.py
@@ -6,9 +6,10 @@ import sys
 import cv2 as cv
 import faceGestureRecognitionV2
 import threading
+import numpy as np
 
 
-class FaceTracking:
+class FaceTracker:
     face = None
     cap = None
 
@@ -16,9 +17,7 @@ class FaceTracking:
     tracker_eyeL = None
     tracker_eyeR = None
 
-    roi_eyeL = None
-    roi_eyeR = None
-    roi_head = None
+    check_timer = False
 
     tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
     tracker_type = tracker_types[2]
@@ -38,59 +37,41 @@ class FaceTracking:
                                  (face.leftEyeX+face.eyeWidth,face.leftEyeY+face.eyeHeight),
                                  (255,0,0),2)
                 cv.imshow('looking for face', frame)
-                if face.leftEyeX is not None and face.startX is not None:
+                if face.leftEyeX is not None and face.startX is not None and face.rightEyeX is not None:
                     self.face = face
                     self.tracker_head = cv2.TrackerCSRT_create()
                     self.tracker_head.init(frame, (face.startX, face.startY, face.width, face.height))
                     self.tracker_eyeL = cv2.TrackerCSRT_create()
-                    self.tracker_eyeL.init(frame, (face.leftEyeX, face.leftEyeY, (face.width, face.height)))
+                    self.tracker_eyeL.init(frame, (face.leftEyeX, face.leftEyeY, face.eyeWidth, face.eyeHeight))
                     self.tracker_eyeR = cv2.TrackerCSRT_create()
-                    self.tracker_eyeR.init(frame, (face.rightEyeX, face.rightEyeY, (face.width, face.height)))
+                    self.tracker_eyeR.init(frame, (face.rightEyeX, face.rightEyeY, face.eyeWidth, face.eyeHeight))
                     cv.destroyAllWindows()
+
+                    self.timer()
             cv.waitKey(1)
 
     def check_viola_jones(self, frame):
-        foundFaces = faceGestureRecognitionV2.getFaces(frame)
-        if len(foundFaces) != 0:
-            face = foundFaces[0]
+        foundfaces = faceGestureRecognitionV2.getFaces(frame)
+        if len(foundfaces) != 0:
+            face = foundfaces[0]
             if face.startX is not None:
                 self.face.setHead(face.startX, face.startY, face.width, face.height)
+                self.tracker_head = cv2.TrackerCSRT_create()
                 self.tracker_head.init(frame, (face.startX, face.startY, face.width, face.height))
-            if face.leftEyeX is not None and face.startX is not None:
-                self.face.setEyes(face.leftEyeX, face.leftEyeY, face.rightEyeX, face.rightEyeY, face.eyeWidth,
+            if face.rightEyeX is not None:
+                self.face.setEyes(face.leftEyeX, face.leftEyeY, self.face.rightEyeX, self.face.rightEyeY, face.eyeWidth,
                                   face.eyeHeight)
-                self.tracker_eyeR.init(frame, (face.rightEyeX, face.rightEyeY, (face.width, face.height)))
-                self.tracker_eyeL.init(frame, (face.leftEyeX, face.leftEyeY, (face.width, face.height)))
-
-                self.face.setEyes(face.leftEyeX, face.leftEyeY, face.rightEyeX, face.rightEyeY, face.eyeWidth,
+                self.tracker_eyeR = cv2.TrackerCSRT_create()
+                self.tracker_eyeR.init(frame, (face.rightEyeX, face.rightEyeY, face.eyeWidth, face.eyeHeight))
+            if face.leftEyeX is not None:
+                self.face.setEyes(self.face.leftEyeX, self.face.leftEyeY, face.rightEyeX, face.rightEyeY, face.eyeWidth,
                                   face.eyeHeight)
-
-    def Track(self, frame, tracker):
-
-        # Update tracker
-        return tracker.update(frame)
-
-        # Draw bounding box
-        # if ok:
-        #     Tracking success
-
-
-            # p1 = (int(bbox[0]), int(bbox[1]))
-            # p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            # cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
-        # else:
-        #     Tracking failure
-            # cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
-        # Display tracker type on frame
-        # cv2.putText(frame, tracker_type + " Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
-
-        # Display result
-        # cv2.imshow("Tracking", frame)
-
+                self.tracker_eyeL = cv2.TrackerCSRT_create()
+                self.tracker_eyeL.init(frame, (face.leftEyeX, face.leftEyeY, face.eyeWidth, face.eyeHeight))
 
     def timer(self):
         self.check_timer = True
-        threading.Timer(1, self.timer).start()
+        threading.Timer(2, self.timer).start()
 
     def PerformFaceTracking(self):
         ret, frame = self.cap.read()
@@ -100,7 +81,11 @@ class FaceTracking:
         else:
             # https://docs.opencv.org/3.4/db/df8/tutorial_py_meanshift.html
             ok_head, track_window = self.tracker_head.update(frame)
+            track_window = [int(i) for i in track_window]
             self.face.setHead(track_window[0], track_window[1], track_window[2], track_window[3])
-            ok_Leye, track_window_left = self.tracker_eyeL(frame)
-            ok_Reye, track_window_right = self.tracker_eyeR(frame)
-            self.face.setEyes(track_window_left[0], track_window_left[1], track_window_right[0], track_window_right[1], track_window_right[2], track_window_right[3])
+            ok_Leye, track_window_left = self.tracker_eyeL.update(frame)
+            track_window_left = [int(i) for i in track_window_left]
+            ok_Reye, track_window_right = self.tracker_eyeR.update(frame)
+            track_window_right = [int(i) for i in track_window_right]
+            self.face.setEyes(track_window_left[0], track_window_left[1], track_window_right[0], track_window_right[1],
+                              self.face.eyeWidth, self.face.eyeHeight)
--- mainv2.py
+++ mainv2.py
@@ -5,25 +5,21 @@ from HeadTiltDetectorv2 import TiltDetector
 from HeadNodDetection import nodDetector
 from Tracker import FaceTracking
 from ImageController import image_controller
-import MHIv3 as MHI
+import MHIv3 as mhi
+from OpenCVTracker import FaceTracker
 
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
-if vc.isOpened():  # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
-
 frameCount = 0
 faces = None
-facetracker = FaceTracking(vc)
+facetracker = FaceTracker(vc)
 cv2.destroyWindow("preview")
 tiltdetector = TiltDetector()
 noddetector = nodDetector()
-MHI.reset(vc.read()[1])
+mhi.reset(vc.read()[1])
 cv.waitKey(2)
-MHI.nextFrame(vc.read()[1])
+mhi.nextFrame(vc.read()[1])
 
 # any image can be passed along to be displayed
 image_controller = image_controller(vc.read()[1])
@@ -33,7 +29,7 @@ does stuff like tracking face, tracking eyes and such
 """
 def perform_tests():
     ret, frame = vc.read()
-    mhi = MHI.nextFrame(frame)
+    mhi_value = mhi.nextFrame(frame)
     facetracker.PerformFaceTracking()
     # print(str(facetracker.face.startX) + ", " + str(facetracker.face.startY) + ", " + str(facetracker.face.width) + ", " + str(facetracker.face.height))
     # cv2.rectangle(frame,(facetracker.face.startX,facetracker.face.startY),(facetracker.face.startX+facetracker.face.width,facetracker.face.startY+facetracker.face.height),(255,0,0),2)
@@ -49,8 +45,8 @@ def perform_tests():
     # frame = cv.polylines(frame, [pts], True, 255, 2)
     face = facetracker.face
     frame = cv2.rectangle(frame, (face.startX, face.startY), (face.startX + face.width, face.startY + face.height), (255,0,0),2)
-    # frame = cv2.rectangle(frame, (face.leftEyeX, face.leftEyeY), (face.eyeWidth + face.leftEyeX, face.eyeHeight + face.leftEyeY), (255,0,0),2)
-    # frame = cv2.rectangle(frame, (face.rightEyeX, face.rightEyeY), (face.eyeWidth + face.rightEyeX, face.eyeHeight + face.rightEyeY), (255,0,0),2)
+    frame = cv2.rectangle(frame, (face.leftEyeX, face.leftEyeY), (face.eyeWidth + face.leftEyeX, face.eyeHeight + face.leftEyeY), (255,0,0),2)
+    frame = cv2.rectangle(frame, (face.rightEyeX, face.rightEyeY), (face.eyeWidth + face.rightEyeX, face.eyeHeight + face.rightEyeY), (255,0,0),2)
 
     cv2.imshow('preview', frame)
 
@@ -89,8 +85,8 @@ the big while loop that runs continuesly
 while True:
     perform_tests()
     # code for checking head tilt(leftor right)
-    tiltdetector.update(facetracker.headangle)
-    #
+    # tiltdetector.update(facetracker.headangle)
+
     # code for checking head nod(vertical or horizontal)
     noddetector.update(facetracker.face)
     perform_checks()
